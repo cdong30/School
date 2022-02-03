6f26c616e9e327bc7dab300decf20c5817ed7546
cdong30 <98903833+cdong30@users.noreply.github.com>
2022-02-02 22:08:03 -0500

    Add files via upload

---
+++ gpaCalc.java
@@ -0,0 +1,39 @@
+public class gpaCalc{
+	public static void main (String[] args){
+		double insertAverage;
+		insertAverage = 93.25;
+		if (insertAverage >= 93.0){
+			System.out.println("GPA: 4.0");
+		}
+		else if(insertAverage >= 90.0){
+			System.out.println("GPA: 3.7");
+		}
+		else if(insertAverage >= 87.0){
+			System.out.println("GPA: 3.3");
+		}
+		else if(insertAverage >= 83.0){
+			System.out.println("GPA: 3.0");
+		}
+		else if(insertAverage >= 80.0){
+			System.out.println("GPA: 2.7");
+		}
+		else if(insertAverage >= 77.0){
+			System.out.println("GPA: 2.3");
+		}
+		else if(insertAverage >= 73.0){
+			System.out.println("GPA: 2.0");
+		}
+		else if(insertAverage >= 70.0){
+			System.out.println("GPA: 1.7");
+		}
+		else if(insertAverage >= 67.0){
+			System.out.println("GPA: 1.3");
+		}
+		else if(insertAverage >= 65.0){
+			System.out.println("GPA: 1.0");
+		}
+		else if(insertAverage >= 0.0){
+			System.out.println("GPA: 0.0");
+		}
+	}
+}
